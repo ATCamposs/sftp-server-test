8b5b7771b211b3e7fc5ab75a57a66fb81f7e965e
Andre Campos <andre.tatibano@gmail.com>
2024-07-21 19:51:59 -0300

    add logrus

--- go.mod
+++ go.mod
@@ -3,8 +3,12 @@ module sftp-client
 go 1.18
 
 require (
+	github.com/pkg/sftp v1.13.6
+	github.com/sirupsen/logrus v1.9.3
+	golang.org/x/crypto v0.1.0
+)
+
+require (
 	github.com/kr/fs v0.1.0 // indirect
-	github.com/pkg/sftp v1.13.6 // indirect
-	golang.org/x/crypto v0.1.0 // indirect
 	golang.org/x/sys v0.1.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,17 +3,30 @@ package main
 import (
 	"fmt"
 	"io"
-	"log"
 	"os"
 	"path/filepath"
 	"sort"
+	"syscall"
 	"time"
 
 	"github.com/pkg/sftp"
+	"github.com/sirupsen/logrus"
 	"golang.org/x/crypto/ssh"
 )
 
+var log = logrus.New()
+
+func init() {
+	log.SetFormatter(&logrus.TextFormatter{
+		FullTimestamp: true,
+	})
+	log.SetOutput(os.Stdout)
+	log.SetLevel(logrus.DebugLevel)
+}
+
 func main() {
+	start := time.Now()
+
 	// Configurações de conexão SFTP
 	sftpHost := "localhost"
 	sftpPort := 2222
@@ -27,35 +40,56 @@ func main() {
 			ssh.Password(sftpPass),
 		},
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+		Timeout:         30 * time.Second,
 	}
 
 	// Conectar ao servidor SSH
 	conn, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", sftpHost, sftpPort), config)
 	if err != nil {
-		log.Fatalf("Falha ao conectar ao servidor SSH: %v", err)
+		log.WithFields(logrus.Fields{
+			"host": sftpHost,
+			"port": sftpPort,
+		}).Error("Falha ao conectar ao servidor SSH")
+		log.Fatal(err)
 	}
+	log.WithFields(logrus.Fields{
+		"host": sftpHost,
+		"port": sftpPort,
+	}).Info("Conexão SSH estabelecida com sucesso")
 	defer conn.Close()
 
 	// Criar cliente SFTP
 	client, err := sftp.NewClient(conn)
 	if err != nil {
-		log.Fatalf("Falha ao criar cliente SFTP: %v", err)
+		log.Error("Falha ao criar cliente SFTP após conexão SSH bem-sucedida")
+		log.Fatal(err)
 	}
+	log.Info("Cliente SFTP criado com sucesso")
 	defer client.Close()
 
 	// Obter e imprimir o diretório atual
 	pwd, err := client.Getwd()
 	if err != nil {
-		log.Printf("Erro ao obter diretório atual: %v", err)
+		log.WithError(err).Warn("Erro ao obter diretório atual")
 	} else {
-		fmt.Printf("Diretório atual: %s\n", pwd)
+		log.WithField("directory", pwd).Info("Diretório atual")
 	}
+
+	// Verificar permissões do diretório remoto
+	info, err := client.Stat("/upload")
+	if err != nil {
+		log.WithError(err).Error("Erro ao verificar informações do diretório /upload")
+		log.Fatal("Falha ao verificar diretório")
+	}
+	log.WithField("permissions", info.Mode()).Debug("Permissões do diretório /upload")
 
 	// Listar arquivos no diretório remoto
 	files, err := client.ReadDir("/upload")
 	if err != nil {
-		log.Fatalf("Falha ao listar arquivos: %v", err)
+		log.WithError(err).Error("Erro ao tentar ler o diretório /upload")
+		log.Fatal("Falha ao listar arquivos")
 	}
+	log.WithField("fileCount", len(files)).Info("Arquivos lidos do diretório /upload")
 
 	// Ordenar arquivos por data de modificação (mais recente primeiro)
 	sort.Slice(files, func(i, j int) bool {
@@ -63,40 +97,67 @@ func main() {
 	})
 
 	// Imprimir lista de arquivos
-	fmt.Println("Arquivos disponíveis:")
+	log.Info("Arquivos disponíveis:")
 	for _, file := range files {
-		fmt.Printf("%s - %s\n", file.Name(), file.ModTime().Format(time.RFC3339))
+		log.WithFields(logrus.Fields{
+			"fileName": file.Name(),
+			"modTime":  file.ModTime().Format(time.RFC3339),
+		}).Debug("Arquivo encontrado")
 	}
 
 	if len(files) > 0 {
 		// Pegar o arquivo mais recente
 		latestFile := files[0]
-		fmt.Printf("\nBaixando o arquivo mais recente: %s\n", latestFile.Name())
+		log.WithField("fileName", latestFile.Name()).Info("Baixando o arquivo mais recente")
+
+		// Verificar espaço em disco
+		var stat syscall.Statfs_t
+		wd, _ := os.Getwd()
+		syscall.Statfs(wd, &stat)
+		freeSpace := stat.Bavail * uint64(stat.Bsize)
+		if uint64(latestFile.Size()) > freeSpace {
+			log.Error("Espaço insuficiente no disco para baixar o arquivo")
+			return
+		}
+		log.WithField("freeSpace", freeSpace).Debug("Espaço livre no disco")
 
 		// Abrir arquivo remoto
 		fullPath := filepath.Join("/upload", latestFile.Name())
-		fmt.Printf("Tentando abrir arquivo: %s\n", fullPath)
+		log.WithField("filePath", fullPath).Debug("Tentando abrir arquivo remoto")
 		remoteFile, err := client.Open(fullPath)
 		if err != nil {
-			log.Fatalf("Falha ao abrir arquivo remoto: %v", err)
+			log.WithError(err).Error("Erro ao tentar abrir o arquivo remoto")
+			return
 		}
+		log.WithField("filePath", fullPath).Info("Arquivo remoto aberto com sucesso")
 		defer remoteFile.Close()
 
 		// Criar arquivo local
 		localFile, err := os.Create(latestFile.Name())
 		if err != nil {
-			log.Fatalf("Falha ao criar arquivo local: %v", err)
+			log.WithError(err).Error("Erro ao tentar criar o arquivo local")
+			return
 		}
+		log.WithField("fileName", latestFile.Name()).Info("Arquivo local criado com sucesso")
 		defer localFile.Close()
 
 		// Copiar conteúdo do arquivo remoto para o local
-		_, err = io.Copy(localFile, remoteFile)
+		bytesWritten, err := io.Copy(localFile, remoteFile)
 		if err != nil {
-			log.Fatalf("Falha ao copiar arquivo: %v", err)
+			log.WithFields(logrus.Fields{
+				"remoteFile": fullPath,
+				"localFile":  latestFile.Name(),
+			}).Error("Erro durante a cópia do arquivo")
+			return
 		}
+		log.WithFields(logrus.Fields{
+			"fileName":     latestFile.Name(),
+			"bytesWritten": bytesWritten,
+		}).Info("Arquivo copiado com sucesso")
 
-		fmt.Printf("Arquivo %s baixado com sucesso.\n", latestFile.Name())
 	} else {
-		fmt.Println("Nenhum arquivo encontrado.")
+		log.Warn("Nenhum arquivo encontrado")
 	}
+
+	log.WithField("executionTime", time.Since(start)).Info("Tempo total de execução")
 }
