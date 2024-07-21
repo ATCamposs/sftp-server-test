76eebd58ed1c291830b826f97bd267f1d3f198db
Andre Campos <andre.tatibano@gmail.com>
2024-07-21 19:58:06 -0300

    update packs

--- main.go
+++ main.go
@@ -6,22 +6,20 @@ import (
 	"os"
 	"path/filepath"
 	"sort"
-	"syscall"
 	"time"
 
 	"github.com/pkg/sftp"
-	"github.com/sirupsen/logrus"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"golang.org/x/crypto/ssh"
 )
 
-var log = logrus.New()
-
+//Porque usar Zerolog
+//https://betterstack-community.github.io/go-logging-benchmarks/
 func init() {
-	log.SetFormatter(&logrus.TextFormatter{
-		FullTimestamp: true,
-	})
-	log.SetOutput(os.Stdout)
-	log.SetLevel(logrus.DebugLevel)
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339})
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
 }
 
 func main() {
@@ -46,118 +44,115 @@ func main() {
 	// Conectar ao servidor SSH
 	conn, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", sftpHost, sftpPort), config)
 	if err != nil {
-		log.WithFields(logrus.Fields{
-			"host": sftpHost,
-			"port": sftpPort,
-		}).Error("Falha ao conectar ao servidor SSH")
-		log.Fatal(err)
+		log.Fatal().
+			Str("host", sftpHost).
+			Int("port", sftpPort).
+			Err(err).
+			Msg("Falha ao conectar ao servidor SSH")
 	}
-	log.WithFields(logrus.Fields{
-		"host": sftpHost,
-		"port": sftpPort,
-	}).Info("Conexão SSH estabelecida com sucesso")
 	defer conn.Close()
 
 	// Criar cliente SFTP
 	client, err := sftp.NewClient(conn)
 	if err != nil {
-		log.Error("Falha ao criar cliente SFTP após conexão SSH bem-sucedida")
-		log.Fatal(err)
+		log.Fatal().Err(err).Msg("Falha ao criar cliente SFTP")
 	}
-	log.Info("Cliente SFTP criado com sucesso")
 	defer client.Close()
-
-	// Obter e imprimir o diretório atual
-	pwd, err := client.Getwd()
-	if err != nil {
-		log.WithError(err).Warn("Erro ao obter diretório atual")
-	} else {
-		log.WithField("directory", pwd).Info("Diretório atual")
-	}
-
-	// Verificar permissões do diretório remoto
-	info, err := client.Stat("/upload")
-	if err != nil {
-		log.WithError(err).Error("Erro ao verificar informações do diretório /upload")
-		log.Fatal("Falha ao verificar diretório")
-	}
-	log.WithField("permissions", info.Mode()).Debug("Permissões do diretório /upload")
 
 	// Listar arquivos no diretório remoto
 	files, err := client.ReadDir("/upload")
 	if err != nil {
-		log.WithError(err).Error("Erro ao tentar ler o diretório /upload")
-		log.Fatal("Falha ao listar arquivos")
+		log.Fatal().Err(err).Msg("Falha ao listar arquivos")
 	}
-	log.WithField("fileCount", len(files)).Info("Arquivos lidos do diretório /upload")
 
 	// Ordenar arquivos por data de modificação (mais recente primeiro)
 	sort.Slice(files, func(i, j int) bool {
 		return files[i].ModTime().After(files[j].ModTime())
 	})
 
-	// Imprimir lista de arquivos
-	log.Info("Arquivos disponíveis:")
-	for _, file := range files {
-		log.WithFields(logrus.Fields{
-			"fileName": file.Name(),
-			"modTime":  file.ModTime().Format(time.RFC3339),
-		}).Debug("Arquivo encontrado")
+	if len(files) == 0 {
+		log.Warn().Msg("Nenhum arquivo encontrado")
+		return
 	}
 
-	if len(files) > 0 {
-		// Pegar o arquivo mais recente
-		latestFile := files[0]
-		log.WithField("fileName", latestFile.Name()).Info("Baixando o arquivo mais recente")
+	// Criar um pool de workers
+	workerCount := 5
+	jobs := make(chan os.FileInfo, len(files))
+	results := make(chan error, len(files))
 
-		// Verificar espaço em disco
-		var stat syscall.Statfs_t
-		wd, _ := os.Getwd()
-		syscall.Statfs(wd, &stat)
-		freeSpace := stat.Bavail * uint64(stat.Bsize)
-		if uint64(latestFile.Size()) > freeSpace {
-			log.Error("Espaço insuficiente no disco para baixar o arquivo")
-			return
-		}
-		log.WithField("freeSpace", freeSpace).Debug("Espaço livre no disco")
-
-		// Abrir arquivo remoto
-		fullPath := filepath.Join("/upload", latestFile.Name())
-		log.WithField("filePath", fullPath).Debug("Tentando abrir arquivo remoto")
-		remoteFile, err := client.Open(fullPath)
-		if err != nil {
-			log.WithError(err).Error("Erro ao tentar abrir o arquivo remoto")
-			return
-		}
-		log.WithField("filePath", fullPath).Info("Arquivo remoto aberto com sucesso")
-		defer remoteFile.Close()
-
-		// Criar arquivo local
-		localFile, err := os.Create(latestFile.Name())
-		if err != nil {
-			log.WithError(err).Error("Erro ao tentar criar o arquivo local")
-			return
-		}
-		log.WithField("fileName", latestFile.Name()).Info("Arquivo local criado com sucesso")
-		defer localFile.Close()
-
-		// Copiar conteúdo do arquivo remoto para o local
-		bytesWritten, err := io.Copy(localFile, remoteFile)
-		if err != nil {
-			log.WithFields(logrus.Fields{
-				"remoteFile": fullPath,
-				"localFile":  latestFile.Name(),
-			}).Error("Erro durante a cópia do arquivo")
-			return
-		}
-		log.WithFields(logrus.Fields{
-			"fileName":     latestFile.Name(),
-			"bytesWritten": bytesWritten,
-		}).Info("Arquivo copiado com sucesso")
-
-	} else {
-		log.Warn("Nenhum arquivo encontrado")
+	// Iniciar workers
+	for w := 1; w <= workerCount; w++ {
+		go worker(w, client, jobs, results)
 	}
 
-	log.WithField("executionTime", time.Since(start)).Info("Tempo total de execução")
+	// Enviar jobs para os workers
+	for _, file := range files[:min(10, len(files))] { // Limitar a 10 arquivos
+		jobs <- file
+	}
+	close(jobs)
+
+	// Coletar resultados
+	for a := 1; a <= min(10, len(files)); a++ {
+		if err := <-results; err != nil {
+			log.Error().Err(err).Msg("Erro ao baixar arquivo")
+		}
+	}
+
+	log.Info().Dur("executionTime", time.Since(start)).Msg("Tempo total de execução")
+}
+
+func worker(id int, client *sftp.Client, jobs <-chan os.FileInfo, results chan<- error) {
+	for file := range jobs {
+		log.Info().
+			Int("worker", id).
+			Str("file", file.Name()).
+			Msg("Iniciando download")
+
+		err := downloadFile(client, file)
+		results <- err
+	}
+}
+
+func downloadFile(client *sftp.Client, file os.FileInfo) error {
+	remotePath := filepath.Join("/upload", file.Name())
+	localPath := file.Name()
+
+	// Abrir arquivo remoto
+	remoteFile, err := client.Open(remotePath)
+	if err != nil {
+		return fmt.Errorf("erro ao abrir arquivo remoto: %w", err)
+	}
+	defer remoteFile.Close()
+
+	// Criar arquivo local
+	localFile, err := os.Create(localPath)
+	if err != nil {
+		return fmt.Errorf("erro ao criar arquivo local: %w", err)
+	}
+	defer localFile.Close()
+
+	// Pré-alocar espaço no disco
+	if err := localFile.Truncate(file.Size()); err != nil {
+		log.Warn().Err(err).Msg("Falha ao pré-alocar espaço no disco")
+	}
+
+	// Copiar conteúdo com buffer
+	buffer := make([]byte, 32*1024)
+	bytesWritten, err := io.CopyBuffer(localFile, remoteFile, buffer)
+	if err != nil {
+		return fmt.Errorf("erro durante a cópia do arquivo: %w", err)
+	}
+
+	log.Info().
+		Str("file", file.Name()).
+		Int64("bytesWritten", bytesWritten).
+		Msg("Arquivo baixado com sucesso")
+	return nil
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
 }
